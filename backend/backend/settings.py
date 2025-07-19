5ac3aefffe585ac27e5f203ca25720f5518f6522
Tapan Kumar <90493845+tapanKr277@users.noreply.github.com>
2025-07-19 23:31:18 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,5 +182,8 @@ STATICFILES_DIRS = [ ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://lms-frontend-two-lemon.vercel.app/",
+    "lms.tapmad.space",
+]
 
